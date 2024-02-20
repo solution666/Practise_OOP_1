3d7ce6ee60b407541a5c2355f2f25ba9f6af1e55
xsolutionxxx <104066780+solution666@users.noreply.github.com>
2024-02-20 09:52:44 +0200

    Add files via upload

---
+++ Пр2_ООП/script.js
@@ -0,0 +1,30 @@
+class Language {
+    constructor(languageName, proficiencyLevel, currentStatus) {
+        this.languageName
+        this.proficiencyLevel
+        this.currentStatus
+    }
+
+    getLanguageName() {
+        
+    }
+    setLanguageName() {
+        
+    }
+    getProficiencyLevel() {
+        
+    }
+    setProficiencyLevel() {
+        
+    }
+    getCurrentStatus() {
+        
+    }
+    setCurrentStatus() {
+        
+    }
+}
+
+class PersonalInformationForm {
+
+}
